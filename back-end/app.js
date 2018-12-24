7bb33826be3229de010ff7cff65925e313879c68
Peter Daum <pddaum@iupui.edu>
2018-12-24 00:06:59 -0500

    Set headers to allow ember app access Does this mean we won't need CORS? Is CORS better, safer?

--- back-end/app.js
+++ back-end/app.js
@@ -47,4 +47,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// set headers
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  next();
+});
+
 module.exports = app;
