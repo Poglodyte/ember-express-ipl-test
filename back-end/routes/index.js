454a3e1f600ead760e5e4e7a4426c1deb4b87491
Peter Daum <pddaum@iupui.edu>
2018-12-23 23:23:57 -0500

    Added get routes for Manholes

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var manhole_controller = require('../controllers/manholeController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/manholes', manhole_controller.manhole_list);
+router.get('/manhole/:id', manhole_controller.manhole_detail);
 
 module.exports = router;
