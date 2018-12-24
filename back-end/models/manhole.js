ccd7350af9d429dfbf6e99c6b432c402b47a282b
Peter Daum <pddaum@iupui.edu>
2018-12-24 00:16:15 -0500

    Added location as GeoJSON point to Manhole schema

--- back-end/models/manhole.js
+++ back-end/models/manhole.js
@@ -5,6 +5,17 @@ var Schema = mongoose.Schema;
 var ManholeSchema = new Schema(
   {
     name: {type: String, required: true, max: 100},
+    location: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true
+      },
+      coordinates: {
+        type: [Number],
+        required: true
+      }
+    }
   }
 );
 
