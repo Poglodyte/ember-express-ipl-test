b4c517060b6d8b25935252c5978fe7b23a3d9c42
Peter Daum <pddaum@iupui.edu>
2018-12-26 15:11:47 -0500

    Moved /testing to /manholes

--- front-end/tests/unit/routes/manholes-test.js
+++ front-end/tests/unit/routes/manholes-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | testing', function(hooks) {
+module('Unit | Route | manholes', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:testing');
+    let route = this.owner.lookup('route:manholes');
     assert.ok(route);
   });
 });
