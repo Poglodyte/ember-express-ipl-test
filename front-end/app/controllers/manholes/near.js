0c0045de20d18940c28d5ef73462da74f8021c0c
Peter Daum <pddaum@iupui.edu>
2018-12-27 23:03:17 -0500

    Copied create manhole to near query route for now Also now adding markers to the map for each manhole returned from query

--- front-end/app/controllers/manholes/near.js
+++ front-end/app/controllers/manholes/near.js
@@ -4,10 +4,16 @@ export default Controller.extend({
   lng: -86.158,
   lat: 39.768,
   zoom: 17,
+  circle_radius: 20,
+  maxDistance: 20,
 
   init() {
     this._super(...arguments);
     this.selection = {
+      lng: -86.158,
+      lat: 39.768
+    },
+    this.circle_location = {
       lng: -86.158,
       lat: 39.768
     }
@@ -17,11 +23,32 @@ export default Controller.extend({
     submitQuery() {
      // var self = this;
 
-      this.store.query('manhole', { nearLng: this.lng, nearLat: this.lat, minDistance: this.minDistance, maxDistance: this.maxDistance })
+      this.store.query('manhole', { nearLng: this.selection.lng, nearLat: this.selection.lat, minDistance: this.minDistance, maxDistance: this.maxDistance })
         .then((results) => {
           this.set('model', results)  // oh, wow that was simple
          // self.set('results', results);
         //  self.get('model').update();
+        });
+    },
+
+    submitCreate() {
+      let manhole = this.store.createRecord('manhole', {
+        name: this.name,
+        location: { type: "Point", coordinates: [ this.selection.lng, this.selection.lat ] }
+      });
+
+      let self = this;
+
+      manhole.save()
+        .then((response) => {
+          // "response" might be clearer, even though "manhole" works 
+          // (not sure if it's from the response, or this manhole has been updated by the response)
+
+        //  self.transitionToRoute('manholes.detail', response);
+          alert("New manhole added: ", response);
+        })
+        .catch((err) => {
+          // TODO: handle error
         });
     },
 
@@ -33,6 +60,8 @@ export default Controller.extend({
     // });
       this.set('selection.lat', location.lat);
       this.set('selection.lng', location.lng);
+      this.set('circle_location.lat', location.lat);
+      this.set('circle_location.lng', location.lng);
     //  console.log("updating: ", location)
     //  console.log("selection: ", this.selection)
     }
