b79928b3469d6314ae90cd3e643951ec00f6ee72
Peter Daum <pddaum@iupui.edu>
2018-12-29 01:59:55 -0500

    Dragging circle instead, updating based on its center coordinates Dragging circle probably isn't best option in final, maybe drag marker or click/tap to reposition (although clicking/tapping may be used for other interactions)

--- front-end/app/controllers/manholes/near.js
+++ front-end/app/controllers/manholes/near.js
@@ -1,23 +1,36 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
   lng: -86.158,
   lat: 39.768,
   zoom: 17, // 20 seems to be max for Satellite (Indianapolis) and 22 is max for Map
 
+  minDistance: 0,
+  maxDistance: 100,
+
   init() {
     this._super(...arguments);
-    this.marker = {
-      lng: -86.158,
-      lat: 39.768
-    }
+    this.circle = {
+      lng: this.lng,
+      lat: this.lat,
+      inner: this.minDistance,
+      outer: this.maxDistance
+    };
   },
+
+  // was trying to make circle.inner a computed property to convert maxDistance form string to numerical value
+  /*
+  let object = {};
+Ember.defineProperty(object, 'key', Ember.computed(() => 'value'));
+Ember.get(object, 'key') === 'value';
+*/
 
   actions: {
     submitQuery() {
      // var self = this;
 
-      this.store.query('manhole', { nearLng: this.lng, nearLat: this.lat, minDistance: this.minDistance, maxDistance: this.maxDistance })
+      this.store.query('manhole', { nearLng: this.circle.lng, nearLat: this.circle.lat, minDistance: this.minDistance, maxDistance: this.maxDistance })
         .then((results) => {
           this.set('model', results)  // oh, wow that was simple
          // self.set('results', results);
@@ -27,8 +40,12 @@ export default Controller.extend({
 
     onDragend(Event) {
       // Refresh coordinates
-      this.set('marker.lat', Event.target.position.lat());
-      this.set('marker.lng', Event.target.position.lng())
+      this.set('circle.lat', Event.target.center.lat());
+      this.set('circle.lng', Event.target.center.lng());
+    },
+
+    updateRadius() {
+      this.set('circle.outer', Number(this.maxDistance));
     }
   }
 });
