95e5fe3e14153c85c47ff9b00277accdd89c9c05
Peter Daum <pddaum@iupui.edu>
2019-01-12 17:12:27 -0500

    Added document-title addon Also placed AWS keys in .env file, going to test uploading new revision to AWS, commit is needed to generate SHA for revision hash

--- front-end/config/deploy.js
+++ front-end/config/deploy.js
@@ -8,6 +8,19 @@ module.exports = function(deployTarget) {
     ,
     'revision-data': {
       type: 'git-commit'
+    },
+    's3-index': {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      bucket: "ipl-manholes",
+      region: "us-east-2",
+      allowOverwrite: true
+    },
+    's3': {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      bucket: "ipl-manholes",
+      region: "us-east-2"
     }
   };
 
